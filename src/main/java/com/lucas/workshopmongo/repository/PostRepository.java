8993f9f03eb72cf3a54d97c45fc6e36a8901e28a
lkalbuquerque <lukas_@outlook.com>
2019-09-14 14:49:53 -0300

    Consulta simple com @query no formato mongodb

--- src/main/java/com/lucas/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lucas/workshopmongo/repository/PostRepository.java
@@ -4,11 +4,16 @@ import com.lucas.workshopmongo.domain.Post;
 import com.lucas.workshopmongo.domain.User;
 import javafx.geometry.Pos;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
+
+        @Query("{ 'title' :{ $regex: ?0, $options: 'i'} }")
+        List<Post> searchTitle(String text);
+
         List<Post> findByTitleContainingIgnoreCase(String text);
 }
